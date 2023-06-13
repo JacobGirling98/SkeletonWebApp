fd5b34dd992fc382bac3d6cf8cb7386631bc8716
Jacob Girling <jacobgirling@Jacobs-MacBook-Pro.local>
2023-06-13 10:52:00 +0100

    kotlin files

--- src/main/java/com/develogical/QueryProcessor.kt
+++ src/main/java/com/develogical/QueryProcessor.kt
@@ -1,17 +1,21 @@
-package com.develogical;
+package com.develogical
 
-public class QueryProcessor {
+import java.util.*
 
-  public String process(String query) {
+class QueryProcessor {
+    fun process(query: String): String {
+        println("Received query:$query")
 
-    System.out.println("Received query:" + query);
+        if (query.lowercase(Locale.getDefault()).contains("shakespeare")) {
+            return ("William Shakespeare (26 April 1564 - 23 April 1616) was an "
+                    + "English poet, playwright, and actor, widely regarded as the greatest "
+                    + "writer in the English language and the world's pre-eminent dramatist.")
+        } else return ""
 
-    if (query.toLowerCase().contains("shakespeare")) {
-      return "William Shakespeare (26 April 1564 - 23 April 1616) was an "
-          + "English poet, playwright, and actor, widely regarded as the greatest "
-          + "writer in the English language and the world's pre-eminent dramatist.";
+
+
+
+
+
     }
-
-    return "";
-  }
 }
--- src/test/java/com/develogical/QueryProcessorTest.kt
+++ src/test/java/com/develogical/QueryProcessorTest.kt
@@ -1,22 +1,22 @@
-package com.develogical;
+package com.develogical
 
-import static org.hamcrest.CoreMatchers.containsString;
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.MatcherAssert.assertThat;
+import org.hamcrest.CoreMatchers.containsString
+import org.hamcrest.CoreMatchers.`is`
+import org.hamcrest.MatcherAssert.assertThat
+import org.junit.Test
 
-import org.junit.Test;
+class QueryProcessorTest {
+    var queryProcessor = QueryProcessor()
 
-public class QueryProcessorTest {
+    @Test
+    @Throws(Exception::class)
+    fun returnsEmptyStringIfCannotProcessQuery() {
+        assertThat(queryProcessor.process("test"), `is`(""))
+    }
 
-  QueryProcessor queryProcessor = new QueryProcessor();
-
-  @Test
-  public void returnsEmptyStringIfCannotProcessQuery() throws Exception {
-    assertThat(queryProcessor.process("test"), is(""));
-  }
-
-  @Test
-  public void knowsAboutShakespeare() throws Exception {
-    assertThat(queryProcessor.process("Shakespeare"), containsString("playwright"));
-  }
+    @Test
+    @Throws(Exception::class)
+    fun knowsAboutShakespeare() {
+        assertThat(queryProcessor.process("Shakespeare"), containsString("playwright"))
+    }
 }
